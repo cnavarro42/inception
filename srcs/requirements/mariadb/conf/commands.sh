a708aaa78d6afdc9a7160067ffab5c9b10a7cd3e
user <cnavarro9205@gmail.com>
2022-05-10 08:02:52 -0400

    Not env in mariadb container, using arg now

--- srcs/requirements/mariadb/conf/commands.sh
+++ srcs/requirements/mariadb/conf/commands.sh
@@ -1,12 +1,12 @@
-mysql -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DB;"
+mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 
-mysql -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'localhost';"
-mysql -e "SET PASSWORD FOR '$MARIADB_USER'@'localhost' = PASSWORD('$DB_USR_PWD');"
-mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$MARIADB_USER'@'localhost' IDENTIFIED BY '$DB_USR_PWD';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE USER IF NOT EXISTS 'MYSQL_USER'@'localhost';"
+mysql -e "SET PASSWORD FOR 'MYSQL_USER'@'localhost' = PASSWORD('MYSQL_PASSWORD');"
+#mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$MARIADB_USER'@'localhost' IDENTIFIED BY '$DB_USR_PWD';"
+#mysql -e "FLUSH PRIVILEGES;"
 
-mysql -e "CREATE USER IF NOT EXISTS '$DB_SUPER'@'localhost' IDENTIFIED BY '$DB_SUPER_PWD';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_SUPER'@'localhost' WITH GRANT OPTION;"
-mysql -e "FLUSH PRIVILEGES;"
+#mysql -e "CREATE USER IF NOT EXISTS '$DB_SUPER'@'localhost' IDENTIFIED BY '$DB_SUPER_PWD';"
+#mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_SUPER'@'localhost' WITH GRANT OPTION;"
+#mysql -e "FLUSH PRIVILEGES;"
 
-mysql -e "ALTER USER '$R_USR'@'localhost' IDENTIFIED BY '$R_USR_PWD';"
+#mysql -e "ALTER USER '$R_USR'@'localhost' IDENTIFIED BY '$R_USR_PWD';"
